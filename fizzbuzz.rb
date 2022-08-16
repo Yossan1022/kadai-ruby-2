5d3e955e228446f3cc5f3d7e2f5723330d252602
Ubuntu <ubuntu@ip-172-31-50-80.ec2.internal>
2022-08-16 07:37:17 +0000

    first commit

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,20 +1,23 @@
 def fizzbuzz(num)
  
-  if num % 3 == 0
-      puts "Fizz"
+  if num % 3 == 0 && num % 5 == 0
+    puts "FizzBuzz"
+              
+  elsif num % 3 == 0
+    puts "Fizz"
       
-      elsif num % 5 == 0
-          puts "Buzz"
+  elsif num % 5 == 0
+    puts "Buzz"
           
-          elsif num % 3 == 0 && num % 5 == 0
-              puts "FizzBuzz"
-          else
-              puts num
+  else
+    puts num
         
-end
-end
-num_max = 100
-(1...num_max).each do |num|
-puts fizzbuzz(num)
 
-end
+ end
+ end
+ 
+  num_max = 100
+  (1...num_max).each do |num|
+  puts fizzbuzz(num)
+
+ end
